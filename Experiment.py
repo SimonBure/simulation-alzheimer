fef63d5efcd4f5a1e6dcc8c91c7f86746ec880bb
Fallog <fallog@mailo.com>
2024-07-25 23:11:06 +0200

    Adding the class Experiment file

---
+++ Experiment.py
@@ -0,0 +1,39 @@
+import abc
+from Space1D import TimeSpace
+
+
+class Experiment(abc.ABC):
+    start_time: float
+    end_time: float
+
+    def __init__(self, start_experiment: float, end_experiment: float):
+        self.start_time = start_experiment
+        self.end_time = end_experiment
+
+    def get_index_starting_time(self, time_space: TimeSpace) -> int:
+        start_index = time_space.map_value_to_index(self.start_time)
+        return start_index
+
+    def get_index_ending_time(self, time_space: TimeSpace) -> int:
+        end_index = time_space.map_value_to_index(self.end_time)
+        return end_index
+
+    def get_indexes_start_and_end(self, time_space) -> (float, float):
+        start_index = time_space.map_value_to_index(self.start_time)
+        end_index = time_space.map_value_to_index(self.end_time)
+        return start_index, end_index
+
+
+# Shallow class for code clarity
+class Antioxidant(Experiment):
+    pass
+
+
+# Shallow class for code clarity
+class Irradiation(Experiment):
+    pass
+
+
+# Shallow class for code clarity
+class Statin(Experiment):
+    pass
