33c60b6c308e8a47b173e130673f83063c31774e
Fallog <fallog@mailo.com>
2024-07-27 17:14:27 +0200

    Adding a method to create the reaction array for ATM monomers.

--- System.py
+++ System.py
@@ -1,3 +1,4 @@
+import numpy as np
 from numpy import ndarray
 from DensityOverSpace import AtmMonomers, AtmDimers, ApoeProteins, ApoeAtmComplexes
 from Parameter import DiffusionParameter, TransportParameter, FragmentationParameter, PermeabilityParameter
@@ -10,6 +11,7 @@ class ReactionDiffusionAtmApoeSystem:
     dimers: AtmDimers
     apoe_proteins: ApoeProteins
     complexes: ApoeAtmComplexes
+    populations: [AtmMonomers, AtmDimers, ApoeProteins, ApoeAtmComplexes]
 
     k: float
     ka: float
@@ -67,27 +69,51 @@ class ReactionDiffusionAtmApoeSystem:
         self.dimers = AtmDimers(dimers_initial)
         self.apoe_proteins = ApoeProteins(apoe_initial)
         self.complexes = ApoeAtmComplexes(complexes_initial)
+        self.populations = [self.monomers, self.dimers, self.apoe_proteins, self.complexes]
 
-    def compute_dimers_next_density(self, time_simulation_index: int):
-        fragmentation_rate_dimers = (self.ratio_fragmentation_dimers_complexes *
-                                     self.fragmentation_parameter.over_time_values[time_simulation_index])
-        self.dimers.next_values = (self.dimers.actual_values + self.time_space.step *
-                                   (0.5 * self.k * self.monomers.actual_values -
-                                    fragmentation_rate_dimers * self.dimers.actual_values))
+    def setup_populations_every_time_values(self):
+        for pop in self.populations:
+            pop.setup_every_time_values(self.time_space)
 
-    def compute_apoe_next_density(self, time_simulation_index: int):
+    def compute_dimers_next_density(self, time_simulation_index: int) -> ndarray:
+        fragmentation_rate_dimers = (self.ratio_fragmentation_dimers_complexes
+                                     * self.fragmentation_parameter.over_time_values[time_simulation_index])
+        return (self.dimers.actual_values + self.time_space.step * (0.5 * self.k * self.monomers.actual_values
+                                                                    - fragmentation_rate_dimers
+                                                                    * self.dimers.actual_values))
+
+    def compute_apoe_next_density(self, time_simulation_index: int) -> ndarray:
         fragmentation_rate_complexes = self.fragmentation_parameter.over_time_values[time_simulation_index]
-        self.apoe_proteins.next_values = (self.apoe_proteins.actual_values + self.time_space.step *
-                                          (- self.ka * self.monomers.actual_values * self.apoe_proteins.actual_values +
-                                           fragmentation_rate_complexes * self.complexes.actual_values))
+        return (self.apoe_proteins.actual_values + self.time_space.step * (- self.ka * self.monomers.actual_values
+                                                                           * self.apoe_proteins.actual_values
+                                                                           + fragmentation_rate_complexes
+                                                                           * self.complexes.actual_values))
 
-    def compute_complexes_next_density(self, time_simulation_index: int):
+    def compute_complexes_next_density(self, time_simulation_index: int) -> ndarray:
         fragmentation_rate_complexes = self.fragmentation_parameter.over_time_values[time_simulation_index]
-        self.complexes.next_values = (self.complexes.actual_values + self.time_space.step *
-                                      (self.ka * self.monomers.actual_values * self.apoe_proteins.actual_values -
-                                       fragmentation_rate_complexes * self.complexes.actual_values))
+        return (self.complexes.actual_values + self.time_space.step * (self.ka* self.monomers.actual_values
+                                                                       * self.apoe_proteins.actual_values
+                                                                       - fragmentation_rate_complexes
+                                                                       * self.complexes.actual_values))
 
     def compute_bulk_over_nucleus(self) -> float:
-        protein_bulk = float(3.66 * self.monomers.actual_values[0] + 6.98 * self.dimers.actual_values[0] +
-                             self.apoe_proteins.actual_values[0] + 4.66 * self.complexes.actual_values[0])
+        protein_bulk = float(3.66 * self.monomers.actual_values[0] + 6.98 * self.dimers.actual_values[0]
+                             + self.apoe_proteins.actual_values[0] + 4.66 * self.complexes.actual_values[0])
         return protein_bulk
+
+    def create_monomers_reaction_array(self, time_simulation_index: int) -> ndarray:
+        fragmentation_rate = self.fragmentation_parameter.over_time_values[time_simulation_index]
+        reaction_array = np.zeros_like(self.monomers.actual_values)
+
+        reaction_array[1:-1] = (- self.k * self.monomers.actual_values[1:-1] * self.monomers.actual_values[1:-1]
+                                - self.ka * self.monomers.actual_values[1:-1] * self.apoe_proteins.actual_values[1:-1]
+                                + fragmentation_rate * self.complexes.actual_values[1:-1]
+                                + self.ratio_fragmentation_dimers_complexes * fragmentation_rate
+                                * 2 * self.dimers.actual_values[1:-1])
+
+        reaction_array[0] = -self.monomers.actual_values[0]
+        reaction_array[-1] = -self.monomers.actual_values[-1]
+
+        return reaction_array
+
+    # TODO method to assign the next_values of the different populations of the system
