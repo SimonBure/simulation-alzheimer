9ca17b14c22194eba592ceb35063bc8eaec93e4d
Fallog <fallog@mailo.com>
2024-08-01 21:14:15 +0200

    Correcting test_Cell.py

--- agent/tests/test_Cell.py
+++ agent/tests/test_Cell.py
@@ -4,8 +4,8 @@ from agent.Particle import Particle
 
 def test_remove_particle() -> bool:
     a_cell = Cell(0)
-    one_particle = Particle(0, 0)
-    another_particle = Particle(1, 1)
+    one_particle = Particle(0, 0, 0, 0)
+    another_particle = Particle(1, 1, 0, 0)
     a_cell.add_particle(one_particle, another_particle)
     a_cell.remove_particle(one_particle)
 
